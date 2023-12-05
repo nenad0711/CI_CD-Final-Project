b4743a0cbfc85428e77723e003b0e7fe3a1359b9
Nenad <nenad0711@gmail.com>
2023-12-05 13:53:17 -0500

    adding html

---
+++ src/main/java/com/example/MyController.java
@@ -0,0 +1,10 @@
+package com.example;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+@Controller
+public class MyController {
+    @GetMapping("/")
+    public String getIndex() {
+        return "hello";
+    }
+}
