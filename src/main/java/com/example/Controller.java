4ec9a208981498b49ac6c6d55ebe88ab55bee021
Nenad <nenad0711@gmail.com>
2023-12-05 11:07:33 -0500

    adding html

--- src/main/java/com/example/Controller.java
+++ src/main/java/com/example/Controller.java
@@ -1,14 +1,13 @@
 package com.example;
-
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-@GetMapping("/")
-    public String msg(){
-
-        return "WELCOME NENAD";
-
+    @GetMapping("/")
+    public String hello(Model model) {
+        model.addAttribute("message", "Hello, World!");
+        return "hello";
     }
 }
